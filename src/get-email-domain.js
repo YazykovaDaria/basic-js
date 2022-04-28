b14fc180cd1d478e6fb9a56d134b81e2d3af0e0c
YazykovaDaria <yazykovadasva@gmail.com>
2022-04-28 11:40:30 +0300

    domain

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,8 +10,10 @@ const { NotImplementedError } = require('../lib');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+function getEmailDomain(email) {
+  const reqular = /@\b/;
+  const slicer = email.search(reqular);
+  return email.slice(slicer + 1);
 }
 
 module.exports = {
