62ddbd3f07ab354f775c573041b79742fe94fd07
YazykovaDaria <yazykovadasva@gmail.com>
2022-05-01 22:27:46 +0300

    carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,21 +6,25 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity > MODERN_ACTIVITY || sampleActivity <= 0 || isNaN(sampleActivity)) {
+    return false;
+  }
+  const decay = Math.log(MODERN_ACTIVITY / sampleActivity);
+  const age = Math.ceil(decay / (0.693 / HALF_LIFE_PERIOD));
+  return age;
+};
 
 module.exports = {
   dateSample
